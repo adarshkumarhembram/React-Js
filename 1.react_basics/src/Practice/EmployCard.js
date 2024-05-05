08844218558518d8670c27b655da504ba03f968c
adarshkumarhembram <hembramadarshkumar@gmail.com>
2024-05-05 08:54:55 +0530

    coding some react basics concepts

---
+++ 1.react_basics/src/Practice/EmployCard.js
@@ -0,0 +1,10 @@
+function EmployCard(){
+    return(
+        <div>
+            <h1>Adarsh Hembram</h1>
+            <h2><span style={{color:"red"}}>Desigination</span>: Engineering Student</h2>
+            <p><span style={{color:"blue"}}>Experience</span> : <span style={{fontSize:"18px"}}>10 year</span></p>
+        </div>
+    )
+}
+export default EmployCard;
---
+++ 1.react_basics/src/Practice/SumCard.js
@@ -0,0 +1,18 @@
+const headerstyle={color:"green"}
+const headersize={fontSize:"20px"}
+
+function SumCard(){
+    return( <>
+  
+      <h1 style={headerstyle} >Sum of Numbers</h1>
+  
+      <h3 style={headersize} >First number : 5</h3>
+      <h3>Second number :4 </h3>
+  
+      <h5 style={headersize}>Sum: 9</h5>
+  
+    </>
+    )
+  }
+  
+  export default SumCard;
